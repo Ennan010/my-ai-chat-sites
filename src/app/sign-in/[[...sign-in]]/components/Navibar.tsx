fbfd9d5306fa427f82991b3b99928abc1e82012d
Ennan010 <1289499212@qq.com>
2025-08-28 21:24:57 +0800

    Initial commit

---
+++ src/app/sign-in/[[...sign-in]]/components/Navibar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type Props = {
+
+};
+
+const Navibar=(props: Props) => {
+    return (
+        <div>
+                Navibar
+        </div>
+
+)
+}
+export default Navibar;
